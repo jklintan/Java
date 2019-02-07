59f1595ea4990d3799b28a6d8c57fddafbba14b4
Josefine Klintberg <josefine.klintberg@hotmail.com>
2019-02-07 21:01:02 +0100

    working on lab2

---
+++ Dictionary.java
@@ -0,0 +1,86 @@
+package lab2;
+import java.io.*;
+import java.util.*;
+
+public class Dictionary {
+
+	protected BufferedWriter outputWriter;
+	protected ArrayList<Word> theDictionary;
+	
+	//Constructors
+	Dictionary () {
+//	
+//	outputWriter = new BufferedWriter();
+	
+		theDictionary = new ArrayList<Word>();
+	
+	}
+	
+	Dictionary(String input) {
+		this.addWords(input);
+	}
+	
+	
+	
+	
+	public void addWords(String input) {
+	
+		String[] wordArray = input.split(" ");
+	
+		for (String w : wordArray) {
+			Word wordInstance  = new Word(w);
+			theDictionary.add(wordInstance);
+		}
+	
+	}
+	
+	public int numberOfWords() {
+		return theDictionary.size();
+	}
+	
+	public void removeDuplicates() {
+		ArrayList<Word> newList = new ArrayList<Word>();
+		for (Word w : this.theDictionary) {
+			if(!newList.contains(w)) {
+				newList.add(w);
+			}
+		}
+		this.theDictionary = newList;
+	}
+	
+	public String countOccurences() {
+		int maxCount = 1;
+		int M = 0;
+		int N = 1;
+		for (Word w : this.theDictionary) {
+			if (w.counts == N) {
+				M++;
+			} else {
+				if(w.counts > maxCount) {
+					maxCount = w.counts;
+				}
+			}
+		}
+		
+		return "There are " + M + " words that occured " + N + " times";
+	}
+	
+	public void sortDictionaryByCounts() {
+		for(int i = 0; i < theDictionary.size(); i++) {	
+			for(int j = 0; j < theDictionary.size()-1; j++) {	
+				if(theDictionary.get(j).getCounts() <  theDictionary.get(j+1).getCounts()) {							
+					Word temp = theDictionary.get(j);				
+					theDictionary.set(j, theDictionary.get(j+1));				
+					theDictionary.set(j+1, temp);				
+				}
+			}
+		}
+	}
+	
+	public void setFileName(String filename) {
+//		outputWriter
+	}
+	
+
+}
+
